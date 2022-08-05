60c767c7eef52c47d2d9e6e2824bd3ac667eb932
Mohammad Sulthan <muhammadsultana@outlook.com>
2022-08-05 21:32:56 +0700

    Initial Commit

---
+++ In*/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  In*
+//
+//  Created by Mohammad Sulthan on 05/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
